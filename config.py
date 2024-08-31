862d6ff001e3dc0b505d285bd48fc0b6302a8950
Vincenzo392 <ramr78338@gmail.com>
2024-08-31 10:43:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7534174751:AAHJFMWSSTXX5awWppDaK5ruX5EFCuFHWpw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Devilxkanhaiya")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("927996339")
+
+
+
+
+
+
+
+  
